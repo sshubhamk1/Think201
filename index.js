82678cc8e827962e9c5e35098512f7dadfc386ae
Shubham Kumar <sshubhamk1@hotmail.com>
2020-09-12 14:31:46 +0530

    index updated
    the header template is updated with paraterized call so that no
    argument is needed to pass from router

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const app = express()
 const studentRoutes = require('./routes/student')
+const sqlite = require('sqlite3')
+const db = new sqlite.Database('data.db')
 app.use(studentRoutes)
-
-app.use(bodyParser.urlencoded({ extended: true }))
+db.serialize(()=>{
+    db.run( "CREATE TABLE IF NOT EXISTS students (\
+    id INTEGER PRIMARY KEY  AUTOINCREMENT,\
+    name VARCHAR(50),\
+    email VARCHAR(30),\
+    phone VARCHAR(20),\
+    degree VARCHAR(20),\
+    profile_pic VARCHAR(40))")
+});
+db.close()
+app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -14,6 +25,8 @@ app.use(express.static(path.join(__dirname,"node_modules/bootstrap/dist/css/")))
 app.use(express.static(path.join(__dirname, "node_modules/jquery/dist/")))
 app.use(express.static(path.join(__dirname, "node_modules/popper.js/dist/")))
 app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/js/")))
+app.use(express.static(path.join(__dirname,'uploads')))
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
